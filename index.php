ef9bf0f005ab52df4f42005e08d5c51fbaa91e14
berrykot <berrykot@gmail.com>
2020-02-11 19:45:23 +0300

    delete trash

--- index.php
+++ index.php
@@ -1,18 +1,1 @@
 <?php
-
-
-require __DIR__ . '/vendor/autoload.php';
-
-\Slim\Slim::registerAutoloader();
-
-$app = new \Slim\Slim();
-
-$app->get('/films', function(){
-	echo 'Бэтмэн 1';
-});
-
-$app->get('/actors', function(){
-	echo 'Кристиан Бэйл';
-});
-
-$app->run();
